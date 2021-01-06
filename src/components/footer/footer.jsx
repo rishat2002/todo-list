bdc2512c7f3119e38b7f8e60c68a8e457632c090
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2021-01-06 18:13:07 +0300

    new commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import TasksFilter from '../tasks-filter/tasks-filter';
 
 const Footer = ({ count, filterType, changeFilter, clearCompletedTasks }) => {
